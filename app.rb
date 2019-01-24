4dffb67eae44b7dd45906b0179f53fd6810cc13a
Florentbilocq <florentbilocq@gmail.com>
2019-01-24 21:05:11 +0100

    mise à jour

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require 'bundler'
 Bundler.require
-$:.unshift File.expand_path('./../lib', __FILE__)
-require ''
+$:.unshift File.expand_path('./../lib/app', __FILE__)
+require 'game.rb'
+require 'player.rb'
+require 'router.rb'
+require 'controler.rb'
+require 'view.rb'
+
+Router.new.perform
